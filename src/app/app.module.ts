b0f6be96af99e574b90bf11a0ce21398b79ec092
Tigran <go.projects.ogle@gmail.com>
2023-03-13 15:09:31 +0300

    03

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormViewChildComponent } from './form-view-child/form-view-child.component';
+import { FormNgFormParametriesComponent } from './form-ng-form-parametries/form-ng-form-parametries.component';
+import { FormReactiveFormsModuleComponent } from './form-reactive-forms-module/form-reactive-forms-module.component';
+import { FormFormBuilderComponent } from './form-form-builder/form-form-builder.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormAppComponent } from './form-app/form-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormViewChildComponent,
+    FormNgFormParametriesComponent,
+    FormReactiveFormsModuleComponent,
+    FormFormBuilderComponent,
+    FormAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
